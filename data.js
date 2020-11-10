e1aad66541f808681a0759548ea3f2a0c9a4209c
Bryan Mailloux <Bryan.Mailloux@D2L.com>
2020-11-10 16:51:34 -0500

    Switch to highcharts

--- data.js
+++ data.js
@@ -1,22 +1,120 @@
-export function getOrgUnitData() {
-    return `id,name,type,parent
-6606,Org,Organization,
-1,Faculty 1,Faculty,6606
-2,Faculty 2,Faculty,6606
-11,Department 1-1,Department,1
-12,Department 1-2,Department,1
-21,Department 2-1,Department,2
-111,Course 11-1,Course,11
-112,Course 11-2,Course,11
-1111,Course 11-1 Semester 1,Course Offering,111
-1112,Course 11-1 Semester 1,Course Offering,112`;
-}
-
-export function getSemesterData() {
-    return `id,name,type,parent
-6606,Org,Organization,
-1000,Semester 1,Semester,6606
-2000,Semester 2,Semester,6606
-1111,Course 11-1 Semester 1,Course Offering,1000
-1112,Course 11-1 Semester 1,Course Offering,1000`;
+export function getOrgData() {
+    return {
+        orgUnits: [
+            {
+                id: 6606,
+                name: 'Org',
+                type: 'Organization'
+            },
+            {
+                id: 1,
+                name: 'Faculty 1',
+                type: 'Faculty'
+            },
+            {
+                id: 2,
+                name: 'Faculty 2',
+                type: 'Faculty'
+            },
+            {
+                id: 11,
+                name: 'Department 1-1',
+                type: 'Department'
+            },
+            {
+                id: 12,
+                name: 'Department 1-2',
+                type: 'Department'
+            },
+            {
+                id: 21,
+                name: 'Department 2-1',
+                type: 'Department'
+            },
+            // {
+            //     id: 111,
+            //     name: 'Course 11-1',
+            //     type: 'Course'
+            // },
+            // {
+            //     id: 112,
+            //     name: 'Course 11-2',
+            //     type: 'Course'
+            // },
+            // {
+            //     id: 1000,
+            //     name: 'Semester 1',
+            //     type: 'Semester'
+            // },
+            // {
+            //     id: 2000,
+            //     name: 'Semester 2',
+            //     type: 'Semester'
+            // },
+            // {
+            //     id: 1111,
+            //     name: 'Course 11-1 Semester 1',
+            //     type: 'Course Offering'
+            // },
+            // {
+            //     id: 1112,
+            //     name: 'Course 11-2 Semester 1',
+            //     type: 'Course Offering'
+            // },
+        ],
+        structure: [
+            {
+                child: 1,
+                parent: 6606
+            },
+            {
+                child: 2,
+                parent: 6606
+            },
+            {
+                child: 11,
+                parent: 1
+            },
+            {
+                child: 12,
+                parent: 1
+            },
+            {
+                child: 21,
+                parent: 2
+            },
+            // {
+            //     child: 111,
+            //     parent: 11
+            // },
+            // {
+            //     child: 112,
+            //     parent: 11
+            // },
+            // {
+            //     child: 1000,
+            //     parent: 6606
+            // },
+            // {
+            //     child: 2000,
+            //     parent: 6606
+            // },
+            // {
+            //     child: 1111,
+            //     parent: 111
+            // },
+            // {
+            //     child: 1111,
+            //     parent: 1000
+            // },
+            // {
+            //     child: 1112,
+            //     parent: 112
+            // },
+            // {
+            //     child: 1112,
+            //     parent: 1000
+            // },
+        ]
+    };
 }
--- orgStructureGraph.js
+++ orgStructureGraph.js
@@ -1,90 +1,91 @@
 import { html, LitElement } from 'lit-element';
-import { getOrgUnitData } from './data';
-import * as d3 from 'd3';
-import { csvParse } from 'd3-dsv';
-import { cluster, stratify }  from 'd3-hierarchy';
+import {getOrgData} from './data';
+
+import 'highcharts';
+import 'highcharts/modules/data';
+import 'highcharts/modules/sankey';
+import 'highcharts/modules/organization';
 
 class OrgStructureGraph extends LitElement {
 
     constructor() {
         super();
+        // this.highcharts = Highcharts;
 
-        this._data = csvParse(getOrgUnitData());
-        console.log(this._data);
-        const dataStructure = stratify()
-            .id(d => d.id)
-            .parentId(d => d.parent);
+        const rawData = getOrgData();
+        const structureArray = rawData.structure.map(rel => [rel.parent, rel.child]);
 
-        this._hierarchy = dataStructure(this._data)
-            .sort((a, b) => d3.descending(a.height, b.height) || d3.ascending(a.data.name, b.data.name));
+        this.options = {
+            title: { text: 'Org Structure Graph' },
+            series: [{
+                type: 'organization',
+                name: 'Org Structure',
+                keys: ['from', 'to'],
+                data: structureArray,
+                marker: {
+                    symbol: 'square',
+                    radius: 100,
+                },
+                levels: [{
+                    level: 0,
+                    color: 'silver',
+                    dataLabels: {
+                        color: 'black'
+                    },
+                    height: 25
+                }, {
+                    level: 1,
+                    color: 'silver',
+                    dataLabels: {
+                        color: 'black'
+                    },
+                    height: 25
+                }, {
+                    level: 2,
+                    color: '#980104'
+                }, {
+                    level: 4,
+                    color: '#359154'
+                }],
+                nodes: rawData.orgUnits,
+                colorByPoint: false,
+                color: '#007ad0',
+                dataLabels: {
+                    color: 'white'
+                },
+                borderColor: 'white',
+                nodeWidth: 100,
+            }],
+            tooltip: {
+                outside: true
+            },
+            exporting: {
+                allowHTML: true,
+                sourceWidth: 800,
+                sourceHeight: 600
+            }
+        };
 
-        this._graph = cluster().nodeSize([20, 20])(this._hierarchy);
-        console.log(this._graph);
     }
 
-    firstUpdated() {
-        this._createGraph();
+    updated() {
+        // if (this.graph) {
+        //     // this.graph.update(this.options, ...(this.updateArgs || [true, true]));
+        // }
+        // else {
+            this._createGraph();
+        // }
     }
 
     _createGraph() {
-        const svg = d3.select(this.shadowRoot.getElementById('graph-container')).append('svg');
+        const graphContainer = this.shadowRoot.getElementById('graph-container');
+        this.graph = Highcharts.chart(graphContainer, this.options);
 
-        const dy = 10;
-
-        svg.append("g")
-            .attr("fill", "none")
-            .attr("stroke", "#555")
-            .attr("stroke-opacity", 0.4)
-            .attr("stroke-width", 1.5)
-            .selectAll("path")
-            .data(this._graph.links())
-            .join("path")
-            .attr("d", d => `
-        M${d.target.y},${d.target.x}
-        C${d.source.y + dy / 2},${d.target.x}
-         ${d.source.y + dy / 2},${d.source.x}
-         ${d.source.y},${d.source.x}
-      `);
-
-        svg.append("g")
-            .selectAll("circle")
-            .data(this._graph.descendants())
-            .join("circle")
-            .attr("cx", d => d.y)
-            .attr("cy", d => d.x)
-            .attr("fill", d => d.children ? "#555" : "#999")
-            .attr("r", 5);
-
-        svg.append("g")
-            .attr("font-family", "sans-serif")
-            .attr("font-size", 10)
-            .attr("stroke-linejoin", "round")
-            .attr("stroke-width", 3)
-            .selectAll("text")
-            .data(this._graph.descendants())
-            .join("text")
-            .attr("x", d => d.y)
-            .attr("y", d => d.x)
-            .attr("dy", "0.31em")
-            .attr("dx", d => d.children ? -6 : 6)
-            .text(d => d.data.name)
-            .filter(d => d.children)
-            .attr("text-anchor", "end")
-            .clone(true).lower()
-            .attr("stroke", "white");
-
-        svg.attr("viewBox", [-50, -150, 300, 300]);
-    }
-
-    _autoBox() {
-        const {x, y, width, height} = this.shadowRoot.getElementById('graph-container').node().getBBox();
-        return [x, y, width, height];
     }
 
     render() {
 
         return html`
-            <p>Hello!</p>
             <div id="graph-container"></div>
         `;
     }
