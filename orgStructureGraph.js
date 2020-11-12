3d6afabf65484cf269714cb39b86feda22b30325
Bryan Mailloux <Bryan.Mailloux@D2L.com>
2020-11-12 11:48:35 -0500

    Reset to use d3 trees

--- orgStructureGraph.js
+++ orgStructureGraph.js
@@ -1,11 +1,6 @@
 import { css, html, LitElement} from 'lit-element';
 import {fetchData, getMockData} from './data';
 
-import 'highcharts';
-import 'highcharts/modules/data';
-import 'highcharts/modules/sankey';
-import 'highcharts/modules/organization';
-
 class OrgStructureGraph extends LitElement {
 
     static get styles() {
@@ -13,7 +8,7 @@ class OrgStructureGraph extends LitElement {
             css`
                 #graph-container {
                     height: 100%;
-                    width: 100%;                    
+                    width: 100%;
                 }
             `
         ]
@@ -21,94 +16,14 @@ class OrgStructureGraph extends LitElement {
 
     constructor() {
         super();
-        // this.highcharts = Highcharts;
         this._serverData = [];
 
         fetchData().then(data => {
             this._serverData = data;
 
-            const nodes = data.orgUnits.map(item => {
-                return {
-                    id: item[0],
-                    name: item[1]
-                }
-            });
-
-            this._options = {
-                title: {text: 'Org Structure Graph'},
-                chart: {
-                    height: 1000,
-                    width: 900,
-                    scrollablePlotArea: {
-                        minHeight: 900,
-                        minWidth: 900
-                    },
-                    panning: {
-                        enabled: true,
-                        type: 'xy'
-                    },
-                    panKey: 'shift',
-                    zoomType: 'xy',
-                    zoomKey: 'ctrl',
-                    reflow: false
-                },
-                series: [{
-                    type: 'organization',
-                    name: 'Org Structure',
-                    keys: ['to', 'from'],
-                    data: data.orgUnitParents,
-                    marker: {
-                        symbol: 'square',
-                        radius: 300,
-                    },
-                    levels: [{
-                        level: 0,
-                        color: 'silver',
-                        dataLabels: {
-                            color: 'black'
-                        },
-                        height: 25
-                    }, {
-                        level: 1,
-                        color: 'silver',
-                        dataLabels: {
-                            color: 'black'
-                        },
-                        height: 25
-                    }, {
-                        level: 2,
-                        color: '#980104'
-                    }, {
-                        level: 4,
-                        color: '#359154'
-                    }],
-                    nodes: nodes,
-                    colorByPoint: false,
-                    color: '#007ad0',
-                    dataLabels: {
-                        color: 'white'
-                    },
-                    borderColor: 'white',
-                    nodeWidth: 100,
-                    nodeHeight: 100,
-                }],
-                tooltip: {
-                    outside: true
-                },
-                exporting: {
-                    allowHTML: true,
-                    sourceWidth: 800,
-                    sourceHeight: 600
-                }
-            };
-
             // TODO: fix timing weirdness
 
-            if (this.graph) {
-                this.graph.update(this._options, true, true);
-            } else {
-                this._createGraph();
-            }
+            this._createGraph();
         });
 
     }
@@ -123,8 +38,7 @@ class OrgStructureGraph extends LitElement {
 
     _createGraph() {
         const graphContainer = this.shadowRoot.getElementById('graph-container');
-        this.graph = Highcharts.chart(graphContainer, this._options);
-
+        // make chart here
     }
 
     render() {
